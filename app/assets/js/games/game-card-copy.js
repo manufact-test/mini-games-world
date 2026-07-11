d46c1b840cfb9e0437ba2221a90eb76daf5065e1
manufact-test <manufact.test@gmail.com>
2026-07-11 19:42:53 +0200

    Add Checkers card copy

--- app/assets/js/games/game-card-copy.js
+++ app/assets/js/games/game-card-copy.js
@@ -1,11 +1,13 @@
 import { TIC_TAC_TOE_META } from './tictactoe/meta.js?v=53';
 import { FOUR_IN_A_ROW_META } from './four-in-a-row/meta.js?v=53';
 import { BATTLESHIP_META } from './battleship/meta.js?v=53';
+import { CHECKERS_META } from './checkers/meta.js?v=57';
 
 const GAME_META = {
   [TIC_TAC_TOE_META.id]: TIC_TAC_TOE_META,
   [FOUR_IN_A_ROW_META.id]: FOUR_IN_A_ROW_META,
   [BATTLESHIP_META.id]: BATTLESHIP_META,
+  [CHECKERS_META.id]: CHECKERS_META,
 };
 
 export function initGameCardCopy(){
