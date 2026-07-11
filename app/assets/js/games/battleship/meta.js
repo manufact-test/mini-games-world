d5f03d3557481c8d18989d6cbe963aa1a15eb581
manufact-test <manufact.test@gmail.com>
2026-07-11 17:55:10 +0200

    Add Battleship client metadata

---
+++ app/assets/js/games/battleship/meta.js
@@ -0,0 +1,6 @@
+export const BATTLESHIP_META = {
+  id: 'battleship',
+  title: 'Морской бой',
+  icon: '⚓',
+  description: 'Найдите и потопите все корабли соперника раньше, чем он уничтожит ваши.',
+};
