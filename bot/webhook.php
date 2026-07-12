08d40c935ffcb6ac3c2d1410936132dcb79c155a
manufact-test <manufact.test@gmail.com>
2026-07-12 12:41:03 +0200

    Route first-time users to button onboarding

--- bot/webhook.php
+++ bot/webhook.php
@@ -4,6 +4,7 @@ require __DIR__ . '/core/bootstrap.php';
 require_once __DIR__ . '/helpers/AdminPaymentRejectGuard.php';
 require_once __DIR__ . '/helpers/AdminGoldTopupNotificationGuard.php';
 require_once __DIR__ . '/helpers/AdminSystemCheckGuard.php';
+require_once __DIR__ . '/helpers/UserWelcomeGuard.php';
 
 try {
     $update = json_decode(file_get_contents('php://input') ?: '{}', true);
@@ -16,9 +17,11 @@ try {
     $guard = new AdminPaymentRejectGuard($telegram, $config);
     $goldTopupGuard = new AdminGoldTopupNotificationGuard($telegram, $config);
     $auditGuard = new AdminSystemCheckGuard($telegram, $config);
+    $welcomeGuard = new UserWelcomeGuard($telegram, $config);
     if (!$guard->handle($update)
         && !$goldTopupGuard->handle($update)
-        && !$auditGuard->handle($update)) {
+        && !$auditGuard->handle($update)
+        && !$welcomeGuard->handle($update)) {
         $handler = new WebhookHandler($telegram, $config);
         $handler->handle($update);
     }
