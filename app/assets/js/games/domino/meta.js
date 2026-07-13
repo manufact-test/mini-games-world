9f59effa25aa926b8d85f76a76d4651d0ff7add7
manufact-test <manufact.test@gmail.com>
2026-07-13 11:14:15 +0200

    Add Domino client metadata

---
+++ app/assets/js/games/domino/meta.js
@@ -0,0 +1,7 @@
+export const DOMINO_META = {
+  id: 'domino',
+  title: 'Домино',
+  icon: '🁣',
+  description: 'Соединяйте одинаковые числа и первым избавьтесь от всех костяшек.',
+  boardSize: 7,
+};
