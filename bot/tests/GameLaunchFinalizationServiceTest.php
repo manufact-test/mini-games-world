75b2391491241e145df0cee16aca2b9fd44019ce
manufact-test <manufact.test@gmail.com>
2026-08-08 13:11:50 +0200

    Test TTT finalizer with project clock helper

--- bot/tests/GameLaunchFinalizationServiceTest.php
+++ bot/tests/GameLaunchFinalizationServiceTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once dirname(__DIR__) . '/helpers/response.php';
+require_once dirname(__DIR__) . '/helpers/validators.php';
 require_once dirname(__DIR__) . '/services/GameLaunchFinalizationService.php';
 
 $assertions = 0;
