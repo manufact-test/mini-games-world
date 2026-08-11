e0cd4af61dc956dc3142467ee3a200806f966051
manufact-test <manufact.test@gmail.com>
2026-08-11 23:09:58 +0300

    Shield King V111: bump consolidated review render identity

--- app/v110.php
+++ app/v110.php
@@ -45,7 +45,7 @@ $html = str_replace(
 );
 $html = str_replace(
     './assets/css/main.css?v=92',
-    './assets/css/main.css?v=129&sk=3&icons=c1efd5af&render=9&frame=emblem-cut',
+    './assets/css/main.css?v=130&sk=3&icons=c1efd5af&render=10&review=consolidated',
     $html
 );
 $html = str_replace(
@@ -72,7 +72,8 @@ header('X-MGW-Api-Session-Graph: v1131');
 header('X-MGW-Notification-Graph: v1137');
 header('X-MGW-Invite-Graph: v1137');
 header('X-MGW-Phase-B-Presentation: v123-v110-deterministic-loader');
+header('X-MGW-App-Entry-Presentation: shield-king-v111-borderless-assembly');
 header('X-MGW-Design-System: shield-king-v2-light-metallic');
 header('X-MGW-Icon-Pack: c1efd5afbf0125a090b1755fed2b40cb2cc6f2e1');
-header('X-MGW-Icon-Render: accepted-v9-emblem-cut-frame');
+header('X-MGW-Icon-Render: accepted-v111-consolidated-review');
 echo $html;
