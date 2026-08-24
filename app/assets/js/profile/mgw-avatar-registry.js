2cc7449c87c112eb0addbfd57489cba38b934fd8
manufact-test <manufact.test@gmail.com>
2026-08-24 10:42:46 +0300

    MVP-19.4: cache-bust full fantasy avatar sprite

--- app/assets/js/profile/mgw-avatar-registry.js
+++ app/assets/js/profile/mgw-avatar-registry.js
@@ -1,4 +1,5 @@
-const AVATAR_SPRITE_ASSET = './assets/media/avatars/mgw-avatar-characters-v1.svg?v=2';
+// Previous accepted sprite cache key: mgw-avatar-characters-v1.svg?v=2
+const AVATAR_SPRITE_ASSET = './assets/media/avatars/mgw-avatar-characters-v1.svg?v=3';
 
 export const AVATAR_VISUAL_REGISTRY = Object.freeze({
   'starter-default-01': Object.freeze({ name:'Nova', rarity:'free', asset:AVATAR_SPRITE_ASSET, spriteIndex:0 }),
