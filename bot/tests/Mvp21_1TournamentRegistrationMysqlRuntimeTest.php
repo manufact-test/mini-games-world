6512a204ae554227377ba8ca451904a12a072737
manufact-test <manufact.test@gmail.com>
2026-09-20 21:45:25 +0300

    Tests: include tournament rules copy migration in MySQL runtime

--- bot/tests/Mvp21_1TournamentRegistrationMysqlRuntimeTest.php
+++ bot/tests/Mvp21_1TournamentRegistrationMysqlRuntimeTest.php
@@ -61,6 +61,7 @@ SQL);
 (require $root . '/database/migrations/20260717_0005_create_balances_ledger_reservations.php')->up($db);
 (require $root . '/database/migrations/20260920_0049_create_official_tournaments.php')->up($db);
 (require $root . '/database/migrations/20260920_0050_add_tournament_rules_consent.php')->up($db);
+(require $root . '/database/migrations/20260920_0051_refresh_tournament_rules_copy.php')->up($db);
 
 $db->execute(<<<'SQL'
 CREATE TABLE mgw_runtime_primary_state (
