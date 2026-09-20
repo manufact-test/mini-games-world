c27da078ba6df0d9b739f49669689b747c6c3d94
manufact-test <manufact.test@gmail.com>
2026-09-20 21:50:27 +0300

    MVP-21.2: make rules copy migration self-loading in standalone runners

--- bot/database/migrations/20260920_0051_refresh_tournament_rules_copy.php
+++ bot/database/migrations/20260920_0051_refresh_tournament_rules_copy.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+require_once dirname(__DIR__, 2) . '/tournaments/TournamentRegistrationService.php';
+
 return new class implements DatabaseMigrationInterface {
     public function version(): string
     {
