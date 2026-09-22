52bd8f25b717424e5dfec2d619be13750719acb5
manufact-test <manufact.test@gmail.com>
2026-09-22 12:24:35 +0300

    Apply tournament registration publication migration in tests

--- bot/tests/Mvp21_3ManualAcceptanceRosterFixtureTest.php
+++ bot/tests/Mvp21_3ManualAcceptanceRosterFixtureTest.php
@@ -49,6 +49,7 @@ $db = new PdoDatabaseConnection($pdo);
 (require $root . '/database/migrations/20260920_0049_create_official_tournaments.php')->up($db);
 (require $root . '/database/migrations/20260920_0050_add_tournament_rules_consent.php')->up($db);
 (require $root . '/database/migrations/20260920_0051_refresh_tournament_rules_copy.php')->up($db);
+(require $root . '/database/migrations/20260922_0056_add_tournament_registration_publication.php')->up($db);
 (require $root . '/database/migrations/20260921_0052_add_tournament_schedule.php')->up($db);
 
 $ledger = new LedgerWriteService($db);
